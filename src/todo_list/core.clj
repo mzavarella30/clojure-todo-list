36228a45321e2578fe08b318bd0f4f9c95a1a810
Michael Zavarella <zavarella.dev@gmail.com>
2016-11-18 16:18:59 -0500

    Added dev main function.

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,5 +1,6 @@
 (ns todo-list.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 
 (defn welcome
@@ -11,6 +12,13 @@
     {:status 404
      :body "<p>OOOOOOOOOOOOOOOOOOps</p>"
      :headers {}}))
+
+(defn -dev-main
+  "Simple web server using Ring & Jetty. Utilizes the wrap-reload middleware
+  to allow for easier development."
+  [port-number]
+  (jetty/run-jetty (wrap-reload #'welcome)
+                   {:port (Integer. port-number)}))
 
 (defn -main
   "Simple web server using Ring & Jetty"
