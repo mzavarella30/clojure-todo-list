a797d6347dd098e43e712051c68bdff9283f6aa8
Michael Zavarella <zavarella.dev@gmail.com>
2016-11-18 16:05:32 -0500

    Added welcome function and main to run jetty. welcome is unused for this commit.

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,8 +1,19 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn welcome
+  [request]
+  {:status 200
+   :body "<p>Hello mothafucka</p>"
+   :headers {}})
+
+(defn -main
+  "Simple web server using Ring & Jetty"
+  [port-number]
+  (jetty/run-jetty
+   (fn [request] {:status 200
+                  :body "<p>Hello World!!!</p>"
+                  :headers {}})
+   {:port (Integer. port-number)}))
 
