73d1a0f7334e37923dab4008e855f2d4bb01b210
Michael Zavarella <zavarella.dev@gmail.com>
2016-11-18 16:18:49 -0500

    Added dev profile.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://opensource.org/license/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.5.0"]]
-  :main todo-list.core)
+  :main todo-list.core
+  :profiles {:dev {:main todo-list.core/-dev-main}})
