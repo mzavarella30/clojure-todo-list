21a15a23c4d08464084ad31f1676cdd122a02d15
Michael Zavarella <zavarella.dev@gmail.com>
2016-11-18 16:57:00 -0500

    Added compojure!

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,30 +1,45 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
-            [compjure.core :refer [defroutes GET]]
-            [compjure.route :refer [not-found]]]))
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
+(defroutes app
+  (GET "/" [] welcome)
+  (GET "/goodbye" [] goodbye)
+  (GET "/about" [] about)
+  (GET "/request-info" [] handle-dump)
+  (not-found "Oops..."))
+
+(defn about
+  [request]
+  {:status 200
+   :body "About me!"
+   :headers {}})
+
+(defn goodbye
+  [request]
+  {:status 200
+   :body "Goodbye"
+   :headers {}})
 
 (defn welcome
   [request]
-  (if (= "/" (:uri request))
-    {:status 200
-     :body "<p>Hello mothafucka</p>"
-     :headers {}}
-    {:status 404
-     :body "<p>OOOOOOOOOOOOOOOOOOps</p>"
-     :headers {}}))
+  {:status 200
+   :body "<p>Hello world</p>"
+   :headers {}})
 
 (defn -dev-main
   "Simple web server using Ring & Jetty. Utilizes the wrap-reload middleware
   to allow for easier development."
   [port-number]
-  (jetty/run-jetty (wrap-reload #'welcome)
+  (jetty/run-jetty (wrap-reload #'app)
                    {:port (Integer. port-number)}))
 
 (defn -main
   "Simple web server using Ring & Jetty"
   [port-number]
-  (jetty/run-jetty welcome
+  (jetty/run-jetty app
    {:port (Integer. port-number)}))
 
