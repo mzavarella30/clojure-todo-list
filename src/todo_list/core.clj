ec85d16234cbc40c370ce85780005f5467f91d28
Michael Zavarella <zavarella.dev@gmail.com>
2016-11-18 17:31:00 -0500

    Moved defroutes to bottom of namespace

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -5,15 +5,6 @@
             [compojure.route :refer [not-found]]
             [ring.handler.dump :refer [handle-dump]]))
 
-(defroutes app
-  (GET "/" [] welcome)
-  (GET "/goodbye" [] goodbye)
-  (GET "/about" [] about)
-  (GET "/request-info" [] handle-dump)
-  (GET "/hello/:nombre" [] hello)
-  (GET "/calc/:op/:a/:b" [] calc)
-  (not-found "Oops..."))
-
 (def operands {"+" +
                "-" -
                ":" /
@@ -54,6 +45,15 @@
   {:status 200
    :body "<p>Hello world</p>"
    :headers {}})
+
+(defroutes app
+  (GET "/" [] welcome)
+  (GET "/goodbye" [] goodbye)
+  (GET "/about" [] about)
+  (GET "/request-info" [] handle-dump)
+  (GET "/hello/:nombre" [] hello)
+  (GET "/calc/:op/:a/:b" [] calc)
+  (not-found "Oops..."))
 
 (defn -dev-main
   "Simple web server using Ring & Jetty. Utilizes the wrap-reload middleware
