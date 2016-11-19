ff280dbc9d10656fd489826d0fdb6b26394bcd37
Michael Zavarella <zavarella.dev@gmail.com>
2016-11-19 17:16:17 -0500

    Added repl options to project file

--- project.clj
+++ project.clj
@@ -9,9 +9,12 @@
                  [hiccup "1.0.5"]
                  [org.clojure/java.jdbc "0.7.0-alpha1"]
                  [postgresql/postgresql "9.1-901-1.jdbc4"]]
+  :repl-options {:init-ns todo-list.core
+                 :init (println "We are in " *ns*)}
   :main todo-list.core
   :auto-clean false
   :profiles {:dev {:main todo-list.core/-dev-main}
              :uberjar {:aot :all}}
   :min-lein-version "2.0.0"
   :uberjar-name "todo-list.jar")
+
