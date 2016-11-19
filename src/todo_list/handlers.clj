abf61c131df2ef0a7805dd7818926ea028d3fbe2
Michael Zavarella <zavarella.dev@gmail.com>
2016-11-19 12:39:58 -0500

    base todo handler and a header function

--- src/todo_list/handlers.clj
+++ src/todo_list/handlers.clj
@@ -3,11 +3,25 @@
    [hiccup.core]
    [hiccup.page]))
 
+(defn header
+  "builds the universal header"
+  [& opts]
+  (html5
+   [:head (include-css "style.css")]))
+
 (defn welcome
   "Handler to respond with a simple welcome page"
   [request]
   (html5 {:lang "en"}
-         [:head (include-css "style.css")]
-   [:h1 {:class "info"} "Hello, Clojure World!"]
-   [:p "I made you a todo list!"]
-   [:a {:href "/todo" :class "button"} "Check out your todo list"]))
+         (header)
+         [:body
+          [:h1 {:class "info"} "Hello, Clojure World!"]
+          [:p "I made you a todo list!"]
+          [:a {:href "/todo" :class "button"} "Check out your todo list"]]))
+
+(defn todo
+  "Handler to respond with the todo list"
+  [request]
+  (html5 {:lang "en"}
+         [:head]
+         [:body]))
